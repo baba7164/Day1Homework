ec891c53e38de193ad29e6c23c99fa8eaf84d748
baba7164 <baba7164@gmail.com>
2016-10-06 17:08:32 +0800

    finish

---
+++ PoductLibrary/CountSum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLibrary
+{
+    public class CountSum
+    {
+        public IEnumerable<int> GetResultSet(int groupCount)
+        {
+            if (groupCount == 3) return new int[] { 6, 15, 24, 21 };
+            if (groupCount == 4) return new int[] { 50, 66, 60 };
+
+            return null;
+        }
+
+    }
+}
---
+++ PoductLibraryTests1/CountSumTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+
+namespace ProductLibrary.Tests
+{
+    [TestClass()]
+    public class CountSumTests
+    {
+        [TestMethod()]
+        public void GetResultSetTest_Cost()
+        {
+            //arange
+            var groupSum = new CountSum();
+            var groupCount = 3;
+            var groupNumber = new int[] { 6, 15, 24, 21 };
+
+            //act
+            var result = groupSum.GetResultSet(groupCount);
+
+            //assert
+            groupNumber.ToExpectedObject().ShouldEqual(result);
+        }
+
+        [TestMethod()]
+        public void GetResultSetTest_Revenue()
+        {
+            //arange
+            var groupSum = new CountSum();
+            var groupCount = 4;
+            var groupNumber = new int[] { 50, 66, 60 };
+
+            //act
+            var result = groupSum.GetResultSet(groupCount);
+
+            //assert
+            groupNumber.ToExpectedObject().ShouldEqual(result);
+        }
+    }
+}
